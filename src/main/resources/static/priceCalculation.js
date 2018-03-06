31f862f54b663a4b9072b7402bbed75dfed0e2c5
Alex Reshetnikov <reshetnikov.alex93@gmail.com>
2018-03-06 12:36:04 +0300

    minor changes, in progress

--- src/main/resources/static/priceCalculation.js
+++ src/main/resources/static/priceCalculation.js
@@ -9,92 +9,26 @@ function init() {
             center: [51.66149636, 39.20038956],
             zoom: 12,
             controls: []
-        }),
-        // Создадим панель маршрутизации.
-        /*routePanelControl = new ymaps.control.RoutePanel({
-            options: {
-                // Добавим заголовок панели.
-                showHeader: true,
-                title: 'Расчёт доставки'
-            }
-        });*/
-        zoomControl = new ymaps.control.ZoomControl({
-            options: {
-                size: 'small',
-                float: 'none',
-                position: {
-                    bottom: 145,
-                    right: 10
-                }
-            }
         });
+    var geoObjects;//вначале инициализировать данные, а в аджаксе обновлять
 
-    // Пользователь сможет построить только автомобильный маршрут.
-   /* routePanelControl.routePanel.options.set({
-        types: {auto: true},
-        allowSwitch: false,
-        //превращение координат в адреса
-        reverseGeocoding: false
-    });*/
+    function uploadData() {
+        $.ajax({
+            url: "json"
+        }).done(function (data) {
+            geoObjects = ymaps.geoQuery(data)
+                .addToMap(myMap)
+                .applyBoundsToMap(myMap, {
+                    checkZoomRange: true
+                });
 
-    // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
-    /*routePanelControl.routePanel.state.set({
-        fromEnabled: false,
-        from: 'Москва, Льва Толстого 16'
-     });*/
-
-    //myMap.controls.add(routePanelControl).add(zoomControl);
-    myMap.controls.add(zoomControl);
-
-    var geoObjects;
-
-
-    $.ajax({
-        url: "json"
-    }).done(function(data) {
-        geoObjects = ymaps.geoQuery(data)
-            .addToMap(myMap)
-            .applyBoundsToMap(myMap, {
-                checkZoomRange: true
-            });
-        console.log(geoObjects)
-    });
- console.log(geoObjects)
-    // Получим ссылку на маршрут.
-    /*routePanelControl.routePanel.getRouteAsync().then(function (route) {
-
-        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
-        route.model.setParams({results: 1}, true);
-
-        // Повесим обработчик на событие построения маршрута.
-        route.model.events.add('requestsuccess', function () {
-
-            var activeRoute = route.getActiveRoute();
-            if (activeRoute) {
-                // Получим протяженность маршрута.
-                var length = route.getActiveRoute().properties.get("distance"),
-                    // Вычислим стоимость доставки.
-                    price = calculate(Math.round(length.value / 1000)),
-                    // Создадим макет содержимого балуна маршрута.
-                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
-                        '<span>Расстояние: ' + length.text + '.</span><br/>' +
-                        '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + price + ' р.</span>');
-                // Зададим этот макет для содержимого балуна.
-                route.options.set('routeBalloonContentLayout', balloonContentLayout);
-                // Откроем балун.
-                activeRoute.balloon.open();
-            }
         });
-
-    });*/
-
-    // Функция, вычисляющая стоимость доставки.
-    function calculate(routeLength) {
-        return Math.max(routeLength * DELIVERY_TARIFF, MINIMUM_COST);
     }
 
+    uploadData();
+    console.log(geoObjects);
+
     document.getElementById('uploadButton').onclick = function () {
-        // Для уничтожения используется метод destroy.
         var myPlacemark = new ymaps.Placemark([51.67079450, 39.18878232],
             {
                 balloonContent: "Машина x111xx36",
@@ -133,4 +67,5 @@ function init() {
         // Добавляем мультимаршрут на карту.
         myMap.geoObjects.add(multiRoute);
     };
+
 }
